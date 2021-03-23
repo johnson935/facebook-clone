6b8933c413b8d340579d022f8cd97537ed934712
johnson935 <33596645+johnson935@users.noreply.github.com>
2021-03-23 10:14:54 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,5 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBS4kUrJDpg1pX1Iw-ivNqLlU83jPjtXzI",
-    authDomain: "facebook-1e30f.firebaseapp.com",
-    projectId: "facebook-1e30f",
-    storageBucket: "facebook-1e30f.appspot.com",
-    messagingSenderId: "210015119513",
-    appId: "1:210015119513:web:b7590940fbb623bb063008",
-    measurementId: "G-HKSY06TTVT"
-  };
-
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
